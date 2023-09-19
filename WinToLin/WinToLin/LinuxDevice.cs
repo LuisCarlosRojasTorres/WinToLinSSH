bc10c971ab89866521e9b5230ca0f4f842591487
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2023-09-19 06:20:16 -0300

    Added LinuxDeviceJson

---
+++ WinToLin/WinToLin/LinuxDevice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinToLin
+{
+    public class LinuxDevice
+    {
+        string DeviceIP { get; set; }
+        string DeviceName { get; set; }
+        string DeviceKey { get; set; }
+
+        public LinuxDevice()
+        { 
+            this.DeviceIP = "-";
+            this.DeviceName = "-";
+            this.DeviceKey = "-";
+        }
+    }
+
+
+}
---
+++ WinToLin/WinToLin/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using WinToLin;
+
+Dictionary<string, string> ihmProperties;
+
+using (StreamReader file = File.OpenText(Path.Combine("LinuxDeviceConfig.json")))
+{
+    ihmProperties = JsonConvert.DeserializeObject<Dictionary<string, string>>(file.ReadToEnd());
+    Console.WriteLine("Lobo");
+}
+
